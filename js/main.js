e9546eacbb15a9cdd4021d715eb0be5eecb1b823
Olena-Rudnieva <rudnievao@gmail.com>
2023-05-01 11:48:09 +0300

    Add photos

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// const sliderEl = document.querySelector(".hero-list");
+const nextEl = document.querySelector('.next');
+const prevEl = document.querySelector('.prev');
+
+const mySiema = new Siema({
+  selector: '.hero-list',
+  loop: true,
+});
+
+function onNextClick() {
+  mySiema.next();
+}
+
+function onPrevClick() {
+  mySiema.next();
+}
+
+nextEl.addEventListener('click', onNextClick);
+prevEl.addEventListener('click', onPrevClick);
+
+const lightbox = new SimpleLightbox('.gallery a', {});
